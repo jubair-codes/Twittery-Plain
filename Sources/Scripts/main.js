e52dd701c96d5b5414f8f89b49a28a84cdee80b1
jubair-codes <alpha01708@gmail.com>
2025-04-30 23:08:24 +0600

    working on responsiveness

--- Sources/Scripts/main.js
+++ Sources/Scripts/main.js
@@ -1,3 +1,18 @@
+const sideMenu = document.getElementById("side-menu");
+const menuBtn = document.getElementById("menu-btn");
+const closeMenuBtn = document.getElementById("close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.transform= "translateX(0%)";
+});
+
+closeMenuBtn.addEventListener('click', () => {
+    sideMenu.style.transform= "translateX(100%)";
+});
+
+
+// faq container handler
+
 const faqItems = document.querySelectorAll('.faq-item');
 
 faqItems.forEach(item => {
